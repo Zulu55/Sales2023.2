2541a9e5242af49a958ad232c83b4cb6315dd381
Juan Carlos Zuluaga <juan.zuluaga@cscgfm.com>
2023-08-21 09:56:04 -0500

    Apply code maid

--- Sales/Sales.Backend/Controllers/CountriesController.cs
+++ Sales/Sales.Backend/Controllers/CountriesController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Sales.Backend.Data;
 using Sales.Shared.Entities;
