f50189c02122c3f0e9a5ca25bcf231f080efd675
Juan Carlos Zuluaga <juan.zuluaga@cscgfm.com>
2023-09-16 11:25:40 -0500

    Add user tables

--- Sales/Sales.Backend/Program.cs
+++ Sales/Sales.Backend/Program.cs
@@ -1,11 +1,14 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Sales.Backend.Data;
+using Sales.Backend.Helpers;
 using Sales.Backend.Intertfaces;
 using Sales.Backend.Repositories;
 using Sales.Backend.Services;
 using Sales.Backend.UnitsOfWork;
+using Sales.Shared.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +22,20 @@ builder.Services.AddScoped(typeof(IGenericUnitOfWork<>), typeof(GenericUnitOfWor
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped<IApiService, ApiService>();
 builder.Services.AddTransient<SeedDb>();
+
+builder.Services.AddIdentity<User, IdentityRole>(x =>
+{
+    x.User.RequireUniqueEmail = true;
+    x.Password.RequireDigit = false;
+    x.Password.RequiredUniqueChars = 0;
+    x.Password.RequireLowercase = false;
+    x.Password.RequireNonAlphanumeric = false;
+    x.Password.RequireUppercase = false;
+})
+    .AddEntityFrameworkStores<DataContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddScoped<IUserHelper, UserHelper>();
 
 var app = builder.Build();
 SeedData(app);
@@ -40,6 +57,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
